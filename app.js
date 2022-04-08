1d0d4a4f3932e4540d9ccf2a658e7142d97ea1a1
C:/Program Files/Git/Alen-Rayen <C:/Program Files/Git/alen-rayen@hotmail.com>
2022-04-08 10:45:01 -0300

    concesionarias

---
+++ app.js
@@ -0,0 +1,6 @@
+let concesionaria = require('./modulo/moduloConcesionaria');
+
+//console.log(concesionaria.autos)
+//console.log(concesionaria.buscarAuto("ABC584"))
+//console.log(concesionaria.venderAuto("ABC584"))
+console.log(concesionaria.autosParaLaVenta())
